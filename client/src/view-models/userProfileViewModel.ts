11d115265ffb467facb43b211a7999f189c00170
afdol-LN <afdolleenud63@gmail.com>
2026-07-07 13:17:52 +0700

    sbfjbsfjbdskjf

---
+++ client/src/view-models/userProfileViewModel.ts
@@ -0,0 +1,15 @@
+import { User } from "@/models/user.model";
+import { useState } from "react";
+
+
+export function userProfileViewPoint(){
+    const [user, setUser] = useState<User>();
+
+    useEffect(()=>{
+
+    })
+
+    const UserById = () =>{
+        const result = await 
+    }
+}
