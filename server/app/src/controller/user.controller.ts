9cfdf767ad2b4727f39d1be4b51ad5eae90e9009
afdol-LN <afdolleenud63@gmail.com>
2026-07-11 22:03:45 +0700

    [feat] register

--- server/app/src/controller/user.controller.ts
+++ server/app/src/controller/user.controller.ts
@@ -1,21 +1,30 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-import { BaseController } from "./base.controller";
-import { Userprofile } from "src/entity/userprofile.entity";
-import { userProfileService } from "src/service/user.service";
-import { responseUser } from "src/type/user.interface";
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { CreateUserprofileDto } from 'src/dto/userprofile.dto';
+import { Userprofile } from 'src/entity/userprofile.entity';
+import { authService } from 'src/service/auth.service';
+import { userProfileService } from 'src/service/user.service';
+import { responseUser } from 'src/type/user.interface';
+import { BaseController } from './base.controller';
 
 @Controller('/userprofile')
 export class userController extends BaseController<Userprofile> {
-    constructor(
-        private readonly userService: userProfileService,
-    ) {
-        super(userService);
-    }
+  constructor(
+    private readonly userService: userProfileService,
+    private readonly auth: authService,
+  ) {
+    super(userService);
+  }
 
-    @Get('campus/:campusId')
-    async findUserWithcampus(
-        @Param('campusId', ParseIntPipe) campusId: number,
-    ): Promise<responseUser[]> {
-        return await this.userService.findUserWithcampus(campusId);
-    }
+  @Get('campus/:campusId')
+  async findUserWithcampus(
+    @Param('campusId', ParseIntPipe) campusId: number,
+  ): Promise<responseUser[]> {
+    return await this.userService.findUserWithcampus(campusId);
+  }
+
+  @Post('/register')
+  async registerUser(@Body() userprofile: CreateUserprofileDto) {
+    return await this.auth.register(userprofile);
+  }
+
 }
