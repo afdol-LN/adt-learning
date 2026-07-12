eacb88ced3b20dec5a1679919c62b1a0be6b485e
afdol-LN <afdolleenud63@gmail.com>
2026-07-12 15:18:44 +0700

    [Update} migrate

---
+++ server/app/src/migrations/1783783808718-UpdateSchem.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateSchem1783783808718 implements MigrationInterface {
+    name = 'UpdateSchem1783783808718'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "exercise" DROP CONSTRAINT "FK_4d606e0535d4660e72378ee51c7"`);
+        await queryRunner.query(`ALTER TABLE "exercise" ALTER COLUMN "skill_id" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "exercise" ADD CONSTRAINT "FK_4d606e0535d4660e72378ee51c7" FOREIGN KEY ("skill_id") REFERENCES "skill"("skill_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "exercise" DROP CONSTRAINT "FK_4d606e0535d4660e72378ee51c7"`);
+        await queryRunner.query(`ALTER TABLE "exercise" ALTER COLUMN "skill_id" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "exercise" ADD CONSTRAINT "FK_4d606e0535d4660e72378ee51c7" FOREIGN KEY ("skill_id") REFERENCES "skill"("skill_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
